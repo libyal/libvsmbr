a9946efd4f6285d0032af5e571f1ee10ca82f432
Joachim Metz <joachim.metz@gmail.com>
2019-07-31 06:42:34 +0200

    Applied updates

--- tests/vsmbr_test_memory.c
+++ tests/vsmbr_test_memory.c
@@ -35,15 +35,15 @@
 
 #if defined( HAVE_VSMBR_TEST_MEMORY )
 
-static void *(*vsmbr_test_real_malloc)(size_t)                 = NULL;
-static void *(*vsmbr_test_real_memcpy)(void *, void *, size_t) = NULL;
-static void *(*vsmbr_test_real_memset)(void *, int, size_t)    = NULL;
-static void *(*vsmbr_test_real_realloc)(void *, size_t)        = NULL;
+static void *(*vsmbr_test_real_malloc)(size_t)                       = NULL;
+static void *(*vsmbr_test_real_memcpy)(void *, const void *, size_t) = NULL;
+static void *(*vsmbr_test_real_memset)(void *, int, size_t)          = NULL;
+static void *(*vsmbr_test_real_realloc)(void *, size_t)              = NULL;
 
-int vsmbr_test_malloc_attempts_before_fail                     = -1;
-int vsmbr_test_memcpy_attempts_before_fail                     = -1;
-int vsmbr_test_memset_attempts_before_fail                     = -1;
-int vsmbr_test_realloc_attempts_before_fail                    = -1;
+int vsmbr_test_malloc_attempts_before_fail                           = -1;
+int vsmbr_test_memcpy_attempts_before_fail                           = -1;
+int vsmbr_test_memset_attempts_before_fail                           = -1;
+int vsmbr_test_realloc_attempts_before_fail                          = -1;
 
 /* Custom malloc for testing memory error cases
  * Note this function might fail if compiled with optimation
@@ -82,7 +82,7 @@ void *malloc(
  */
 void *memcpy(
        void *destination,
-       void *source,
+       const void *source,
        size_t size )
 {
 	if( vsmbr_test_real_memcpy == NULL )
