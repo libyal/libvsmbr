fc064341cb3f0d8cf15b1eee0383f192ac5ddfa6
Joachim Metz <joachim.metz@gmail.com>
2020-07-05 15:03:10 +0200

    Applied updates and worked on tests

---
+++ tests/vsmbr_test_tools_signal.c
@@ -0,0 +1,145 @@
+/*
+ * Tools signal functions test program
+ *
+ * Copyright (C) 2010-2020, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+#include <file_stream.h>
+#include <types.h>
+
+#if defined( HAVE_STDLIB_H ) || defined( WINAPI )
+#include <stdlib.h>
+#endif
+
+#include "vsmbr_test_libcerror.h"
+#include "vsmbr_test_macros.h"
+#include "vsmbr_test_unused.h"
+
+#include "../vsmbrtools/vsmbrtools_signal.h"
+
+void vsmbr_test_tools_signal_handler(
+      vsmbrtools_signal_t signal VSMBR_TEST_ATTRIBUTE_UNUSED )
+{
+	VSMBR_TEST_UNREFERENCED_PARAMETER( signal )
+}
+
+/* Tests the vsmbrtools_signal_attach and function
+ * Returns 1 if successful or 0 if not
+ */
+int vsmbr_test_tools_signal_attach(
+     void )
+{
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	result = vsmbrtools_signal_attach(
+	          vsmbr_test_tools_signal_handler,
+	          &error );
+
+	VSMBR_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	VSMBR_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
+}
+
+/* Tests the vsmbrtools_signal_detach and function
+ * Returns 1 if successful or 0 if not
+ */
+int vsmbr_test_tools_signal_detach(
+     void )
+{
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	result = vsmbrtools_signal_detach(
+	          &error );
+
+	VSMBR_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	VSMBR_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
+}
+
+/* The main program
+ */
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+int wmain(
+     int argc VSMBR_TEST_ATTRIBUTE_UNUSED,
+     wchar_t * const argv[] VSMBR_TEST_ATTRIBUTE_UNUSED )
+#else
+int main(
+     int argc VSMBR_TEST_ATTRIBUTE_UNUSED,
+     char * const argv[] VSMBR_TEST_ATTRIBUTE_UNUSED )
+#endif
+{
+	VSMBR_TEST_UNREFERENCED_PARAMETER( argc )
+	VSMBR_TEST_UNREFERENCED_PARAMETER( argv )
+
+#if defined( WINAPI )
+
+	/* TODO add tests for vsmbrtools_signal_handler */
+#endif
+
+#if defined( WINAPI ) && defined( _MSC_VER )
+
+	/* TODO add tests for vsmbrtools_signal_initialize_memory_debug */
+#endif
+
+	VSMBR_TEST_RUN(
+	 "vsmbrtools_signal_attach",
+	 vsmbr_test_tools_signal_attach )
+
+	VSMBR_TEST_RUN(
+	 "vsmbrtools_signal_detach",
+	 vsmbr_test_tools_signal_detach )
+
+	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
+}
+
