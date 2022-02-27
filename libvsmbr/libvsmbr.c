06f0780de2b7d172bec9ada02f66563a7ffcf6e5
Joachim Metz <joachim.metz@gmail.com>
2022-02-27 06:27:11 +0100

    Applied updates

--- libvsmbr/libvsmbr.c
+++ libvsmbr/libvsmbr.c
@@ -1,7 +1,7 @@
 /*
  * Library to access the Master Boot Record (MBR) volume system format
  *
- * Copyright (C) 2010-2021, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2022, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
