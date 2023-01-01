81b524ded30ff282592ac6d9daf4f49b2d7a4d09
Joachim Metz <joachim.metz@gmail.com>
2023-01-01 06:41:08 +0100

    Applied updates and changes for CI tests

--- ossfuzz/partition_fuzzer.cc
+++ ossfuzz/partition_fuzzer.cc
@@ -1,7 +1,7 @@
 /*
  * OSS-Fuzz target for libvsmbr partition type
  *
- * Copyright (C) 2019-2022, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2019-2023, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
