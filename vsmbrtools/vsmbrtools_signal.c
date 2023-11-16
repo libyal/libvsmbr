18e15820903510e6e295994aebb65e132a8693b6
Joachim Metz <joachim.metz@gmail.com>
2023-11-16 06:24:11 +0100

    Applied updates

--- vsmbrtools/vsmbrtools_signal.c
+++ vsmbrtools/vsmbrtools_signal.c
@@ -40,7 +40,7 @@ void (*vsmbrtools_signal_signal_handler)( vsmbrtools_signal_t ) = NULL;
 /* Signal handler for Ctrl+C or Ctrl+Break signals
  */
 BOOL WINAPI vsmbrtools_signal_handler(
-             unsigned long signal )
+             vsmbrtools_signal_t signal )
 {
 	static char *function = "vsmbrtools_signal_handler";
 
@@ -112,7 +112,7 @@ int vsmbrtools_signal_attach(
 	vsmbrtools_signal_signal_handler = signal_handler;
 
 	if( SetConsoleCtrlHandler(
-	     vsmbrtools_signal_handler,
+	     (PHANDLER_ROUTINE) vsmbrtools_signal_handler,
 	     TRUE ) == 0 )
 	{
 		libcerror_error_set(
@@ -179,7 +179,7 @@ int vsmbrtools_signal_detach(
 	static char *function = "vsmbrtools_signal_detach";
 
 	if( SetConsoleCtrlHandler(
-	     vsmbrtools_signal_handler,
+	     (PHANDLER_ROUTINE) vsmbrtools_signal_handler,
 	     FALSE ) == 0 )
 	{
 		libcerror_error_set(
