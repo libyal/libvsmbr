528c201f78bc79e2aac35277aadbc6011a86df61
Joachim Metz <joachim.metz@gmail.com>
2026-09-16 12:04:28 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libvsmbr/.libs/libvsmbr.1.dylib && test -f ./pyvsmbr/.libs/pyvsmbr.so
+if [ -f "${PWD}/libvsmbr/.libs/libvsmbr.1.dylib" ] && [ -f ./pyvsmbr/.libs/pyvsmbr.so ]
 then
-	install_name_tool -change /usr/local/lib/libvsmbr.1.dylib ${PWD}/libvsmbr/.libs/libvsmbr.1.dylib ./pyvsmbr/.libs/pyvsmbr.so
+    install_name_tool -change /usr/local/lib/libvsmbr.1.dylib "${PWD}/libvsmbr/.libs/libvsmbr.1.dylib" ./pyvsmbr/.libs/pyvsmbr.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
